1a884479cdc089a539fcce15ff9e4977b3d2bb5b
Aman Sinha <1998amansinha@gmail.com>
2024-03-07 10:21:01 +0545

    RootLayout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,6 +1,20 @@
+import Bottombar from "@/components/shared/Bottombar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import Topbar from "@/components/shared/Topbar"
+import { Outlet } from "react-router-dom"
+
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section>
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
   )
 }
 
